93e474de1065a8313fc6fef7a4211575f6bbdff2
Lucio Itikawa <lucio.itikawa@benner.com.br>
2017-10-13 14:56:12 -0300

    Adicionando testes unitários (testes de unidade).

---
+++ Semana_17_Generics/Evitando.Duplicacao.de.Codigo/Evitando.Duplicacao.de.Codigo.Tests/Exercicios/ConsultaServicoAutorizacaoTests.cs
@@ -0,0 +1,51 @@
+﻿using Evitando.Duplicacao.de.Codigo.Exercicios;
+using Evitando.Duplicacao.de.Codigo.Generics._2.ComGenerics;
+using Evitando.Duplicacao.de.Codigo.tissLoteGuiasV3_03_02;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Evitando.Duplicacao.de.Codigo.Tests.Exercicios
+{
+
+    [TestFixture]
+    public class ConsultaServicoAutorizacaoTests
+    {
+        [TestCase(1)]
+        [TestCase(11)]
+        [TestCase(21)]
+        [TestCase(1211)]
+        [TestCase(111221)]
+        [TestCase(312211)]
+        [TestCase(13112221)]
+        [TestCase(1113213211)]
+        [TestCase(31131211131221)]
+        public void Deve_consultar_autorizacao(long handleGuia)
+        {
+            var valorDadoBase = handleGuia.ToString();
+            var dadosGuia = Substitute.For<IDadosGuia>();
+            dadosGuia.NumeroGuia.Returns(valorDadoBase);
+            dadosGuia.Beneficiario.Returns(valorDadoBase);
+            dadosGuia.ProfissionalExecutante.Returns(valorDadoBase);
+            var daoBase = Substitute.For<IDAO<IDadosGuia>>();
+            daoBase.Get(Arg.Any<long>()).Returns(dadosGuia);
+
+            var servicoConsulta = new ConsultaServicoAutorizacao(daoBase);
+            var resultadoConsulta = servicoConsulta.Consultar(handleGuia);
+            var resultadoEsperado = new ctm_consultaGuia()
+            {
+                numeroGuiaOperadora = valorDadoBase,
+                dadosBeneficiario = new ct_beneficiarioDados
+                {
+                    nomeBeneficiario = valorDadoBase
+                },
+                profissionalExecutante = new ct_contratadoProfissionalDados
+                {
+                    nomeProfissional = valorDadoBase
+                }
+            };
+
+            resultadoConsulta.ShouldBeEquivalentTo(resultadoEsperado);
+        }
+    }
+}
---
+++ Semana_17_Generics/Evitando.Duplicacao.de.Codigo/Evitando.Duplicacao.de.Codigo.Tests/Exercicios/ValidacaoDocumentoTests.cs
@@ -0,0 +1,46 @@
+﻿using Evitando.Duplicacao.de.Codigo.Exercicios;
+using Evitando.Duplicacao.de.Codigo.Generics._2.ComGenerics;
+using Evitando.Duplicacao.de.Codigo.OO;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Evitando.Duplicacao.de.Codigo.Tests.Exercicios
+{
+    public class EntidadeMock : Entidade<EntidadeMock>, IDadosDocumento
+    {
+        public string ObterCpfCnpj()
+        {
+            return string.Empty;
+        }
+    }
+
+    [TestFixture]
+    public class ValidacaoDocumentoTests
+    {
+        private const string DADO_DOCUMENTO_VALIDO = "123456789";
+        private const string DADO_DOCUMENTO_INVALIDO = "";
+
+        [TestCase(DADO_DOCUMENTO_VALIDO, DADO_DOCUMENTO_VALIDO, "CPF", true, "Sem erro!")]
+        [TestCase(DADO_DOCUMENTO_INVALIDO, DADO_DOCUMENTO_INVALIDO, "CNPJ", false, "Não foi encontrado cnpj em seu cadastro")]
+        [TestCase(DADO_DOCUMENTO_INVALIDO, DADO_DOCUMENTO_VALIDO, "CPF_CNPJ", false, "O cpf/cnpj informado é diferente")]
+        public void Deve_validar_documento(string documentoInformado, string documentoUsuario, string nomeCampo, bool resultado, string mensagem)
+        {
+            var dadosDocumento = Substitute.For<IDadosDocumento>();
+            dadosDocumento.ObterCpfCnpj().Returns(documentoUsuario);
+            var daoBase = Substitute.For<IDAO<IDadosDocumento>>();
+            daoBase.GetFirstOrDefault(Arg.Any<Criteria>()).Returns(dadosDocumento);
+
+            var validador = new ValidacaoDocumento<IDAO<IDadosDocumento>>(daoBase);
+            var resultadoValidacao = validador.Validar(documentoInformado, nomeCampo);
+
+            var resultadoEsperado = new ValidacaoDto()
+            {
+                Resultado = resultado,
+                Mensagem = mensagem
+            };
+
+            resultadoValidacao.ShouldBeEquivalentTo(resultadoEsperado);
+        }
+    }
+}
